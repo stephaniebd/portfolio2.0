e6253c0b6b456d7ccd4ae9b59a820f5829661ff5
Stephanie Beauzile-Domond <stephanie.beauzile.d@gmail.com>
2020-10-24 13:43:35 -0400

    created components and assets folder and imported images, start to work on navbar component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,27 +5,13 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Allo Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Yo </h1>
       <Switch>
           <Route path="/">
             {/* {render home page} */}
