b4bdfa162c16d7e452f270929ab9c02d89f5a435
Stephanie Beauzile-Domond <stephanie.beauzile.d@gmail.com>
2020-10-24 11:04:18 -0400

    created react app portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,33 +1,24 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          
-        </header>
-        <Switch>
-          <Route path="/">
-            {/* {render home page} */}
-          </Route>
-          <Route path="/dont-die-food">
-            {/* {render ddf page} */}
-          </Route>
-          <Route path="/thinktwice">
-            {/* {render tt page} */}
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
