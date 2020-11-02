a9fc90b20108c197c0157db0216d5de964f06b30
Stephanie Beauzile-Domond <stephanie.beauzile.d@gmail.com>
2020-11-02 15:54:26 -0500

    complete footer and links to linkeding and github page

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -8,8 +8,10 @@ const Footer = () => {
             <p>© All right reserved – Stephanie Beauzile-Domond</p>
 
             <ul className="footer-socials">
-                <li><Link activeClassName="" to='/work'>my work</Link></li>
-                <li><Link activeClassName="" to='/contact'>contact</Link></li>
+                <a href="https://www.linkedin.com/in/stephaniebd/" target="blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/stephaniebd" target="blank"><i class="fab fa-github"></i></a>
+                <a href><i class="fab fa-behance"></i></a>  
+                <a href><i class="fab fa-dribbble"></i></a>
             </ul>
         </div>
     )
